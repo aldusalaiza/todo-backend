8c3d1c152a331d50fb7ebd621e85337052f3e467
aldusalaiza <aldus.alaiza@cambridge.org>
2025-07-28 15:06:22 +0800

    add: user model + user field in todo model

--- database/migrations/1753686106277_create_todos_table.ts
+++ database/migrations/1753686106277_create_todos_table.ts
@@ -7,12 +7,20 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
 
-      table.string('title')
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+
+      table.string('title').notNullable()
       table.string('description')
       table.boolean('is_done').defaultTo(false)
 
-      table.timestamp('created_at')
-      table.timestamp('updated_at').defaultTo(this.now())
+      table.timestamp('created_at').notNullable().defaultTo(this.now())
+      table.timestamp('updated_at').notNullable().defaultTo(this.now())
     })
   }
 
