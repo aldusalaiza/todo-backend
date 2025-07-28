f89b50c4279d3f1f7928ea3f44843431dc4c7203
aldusalaiza <aldus.alaiza@cambridge.org>
2025-07-28 14:45:44 +0800

    add: auth middleware

--- start/routes.ts
+++ start/routes.ts
@@ -10,13 +10,18 @@
 
 import TodosController from '#controllers/todos_controller'
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
-router.get('/todos', [TodosController, 'index'])
+router
+  .group(() => {
+    router.get('/todos', [TodosController, 'index'])
 
-router.get('/todo/:id', [TodosController, 'get'])
+    router.get('/todo/:id', [TodosController, 'get'])
 
-router.post('/todo/add', [TodosController, 'store'])
+    router.post('/todo/add', [TodosController, 'store'])
 
-router.delete('/todo/delete', [TodosController, 'destroy'])
+    router.delete('/todo/delete', [TodosController, 'destroy'])
 
-router.put('/todo/edit/:id', [TodosController, 'update'])
+    router.put('/todo/edit/:id', [TodosController, 'update'])
+  })
+  .use(middleware.auth())
